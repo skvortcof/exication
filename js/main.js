2876f2e946f113833346436a1d7695d8e8d44542
skvortcof <40703266+skvortcof@users.noreply.github.com>
2018-09-14 01:36:28 +0300

    preloader

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 (function ($) {
   $(function () {
 
-    $( document ).ready(function() {
-      $('#preloader').fadeOut('slow', function () {
-        $(this).remove();
-      });
-    });
-
   });
 })(jQuery);
 
 
 $( window ).on( "load", function() {
-  console.log( "window loaded" );
+  $('#preloader').fadeOut('slow', function () {
+    $(this).remove();
+  });
 });
