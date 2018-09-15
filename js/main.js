316837d4c5e8c9e7437a7da1d51b7fe18dc8989d
skvortcof <40703266+skvortcof@users.noreply.github.com>
2018-09-16 01:01:46 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
+document.body.classList.add('js');
+
+
 (function ($) {
   $(function () {
 
-    if ($(window).width() > 700) {
-      $('body').addClass('js');
+    if ($(window).width() < 700) {
+      $('body').removeClass('js');
     }
 
   });
 })(jQuery);
 
 $( window ).on('load', function() {
-  $('#preloader').fadeOut('slow', function () {
+  $('#preloader').fadeOut('fast', function () {
     $(this).remove();
   });
 });
